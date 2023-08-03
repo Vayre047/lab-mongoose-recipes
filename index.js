ca611002ec453431315dbddaff0fbe3339e90613
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-08-03 18:10:18 +0100

    Completed lab

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 //Method 1 : Using Async Await
+let secondaryRecipe = {
+  title: 'Arroz de Pato',
+  cuisine: "Portuguese",
+  level: "Hard Stuf",
+  ingredients: "Rice",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/images/75131.jpg",
+  duration: 90,
+  creator: "Paulo Abronhosa",
+  created: "02/08/2021"
+};
 
 const manageRecipes = async () => {
   try {
@@ -19,6 +30,38 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+    // Create
+    const recipeDb = {
+      title: 'Tarte de Peixe',
+      cuisine: "American",
+      level:"Easy Peasy",
+      ingredients: ["fish"],
+      dishType: "breakfast",
+      duration: 80,
+      creator: "Manuel Alegre",
+    };
+
+    // let secondaryRecipeDb = await Recipe.create(secondaryRecipe);
+
+    let createdRecipe = await Recipe.create(recipeDb);
+
+    console.log(createdRecipe);
+
+    let someRecipe = await Recipe.insertMany(data);
+    console.log(someRecipe);
+
+
+
+    let findOneRecipe = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, { new: true, upsert: true });
+console.log(findOneRecipe);
+
+
+    let removeOneRecipe = await Recipe.deleteOne( {title: "Carrot Cake"});
+
+console.log("*****************");
+console.log(findOneRecipe);
+
+mongoose.connection.close();
   } catch (error) {
     console.log(error);
   }
